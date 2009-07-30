a7cd7ad56416880a743b32a28f4f3831ca9e1aed
Nick Fitzgerald <fitzgen@gmail.com>
2009-07-30 00:11:52 -0700

    removed a slash

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
                  Post.objects.filter(is_deleted=False,
                                      is_published=True).order_by("-pub_date")},
             name="blimp_index"),
-        url(r'^/(?P<object_id>\d+)/$', "django.views.generic.list_detail.object_detail", 
+        url(r'^(?P<object_id>\d+)/$', "django.views.generic.list_detail.object_detail", 
             {"queryset": 
                  Post.objects.filter(is_deleted=False,
                                      is_published=True).order_by("-pub_date")},
