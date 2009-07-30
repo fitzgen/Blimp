3b31d8f9b5cc54917dc6787eb958b6a21d092b55
Nick Fitzgerald <fitzgen@gmail.com>
2009-07-29 23:23:16 -0700

    very simple blog post structure

--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    """A blog post or entry or whatever you want to call it."""
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    is_published = models.BooleanField(default=False,
+                                       blank=True,
+                                       null=True)
+    is_deleted = models.BooleanField(default=False,
+                                     blank=True,
+                                     null=True)
+    pub_date = models.DateField()
