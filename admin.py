8c7283a2921d9e100662cd763008ccddf273ae96
Nick Fitzgerald <fitzgen@gmail.com>
2009-07-29 23:24:04 -0700

    adding admin for creating posts

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display = ('title', 'is_published',
+                    'is_deleted', 'pub_date')
+    save_on_top = True
+    search_fields = ('title','content')
+admin.site.register(Post, PostAdmin)
