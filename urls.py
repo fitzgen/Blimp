00502f94b43827ebe32fe53389d103720c74c1f8
Nick Fitzgerald <fitzgen@gmail.com>
2009-07-29 23:41:53 -0700

    too much javascript lately... gotta throw those quotes in

--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import *
+from models import Post
 
 urlpatterns = patterns('',
         url(r'^$', "django.views.generic.list_detail.object_list", 
-            {queryset: 
+            {"queryset": 
                  Post.objects.filter(is_deleted=False,
                                      is_published=True).order_by("-pub_date")},
             name="blimp_index"),
         url(r'^/(?P<object_id>\d+)/$', "django.views.generic.list_detail.object_detail", 
-            {queryset: 
+            {"queryset": 
                  Post.objects.filter(is_deleted=False,
                                      is_published=True).order_by("-pub_date")},
             name="blimp_detail"),
