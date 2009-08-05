30e8db863eea5cf047aa3d161c4324a44c44ed6b
Nick Fitzgerald <fitzgen@gmail.com>
2009-08-05 10:57:07 -0700

    adding contact url (template isn't there yet)

--- urls.py
+++ urls.py
@@ -16,6 +16,9 @@ urlpatterns = patterns('',
     url(r'^$', "django.views.generic.list_detail.object_list", info_dict, name="blimp_index"),
     url(r'^(?P<object_id>\d+)/$', "django.views.generic.list_detail.object_detail", info_dict, name="blimp_detail"),
 
+    # static/direct-to-template
+    url(r'^contact/$', "django.views.generic.simple.direct_to_template", {"template": "Blimp/contact"}, name="blimp_contact"),
+
     # feeds
     url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 )
