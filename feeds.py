da7c4112144772f893d43e7f39ab255dea8a997e
Nick Fitzgerald <fitzgen@gmail.com>
2009-08-05 01:44:58 -0700

    added author name and email

--- feeds.py
+++ feeds.py
@@ -3,6 +3,8 @@ from django.utils.feedgenerator import Atom1Feed
 from models import Post
 
 class RssLatestPosts(Feed):
+    author_name = "Nick Fitzgerald"
+    author_email = "fitzgen@gmail.com"
     title = "Nick Fitzgerald"
     link = "/weblog/"
     description = "Latest posts from fitzgeraldnick.com"
@@ -10,7 +12,8 @@ class RssLatestPosts(Feed):
     def items(self):
         return Post.objects.filter(is_published=True,
                                    is_deleted=False).order_by('-pub_date')[:5]
-                            
+
+
 class AtomLatestPosts(RssLatestPosts):
     feed_type = Atom1Feed
     subtitle = RssLatestPosts.description
