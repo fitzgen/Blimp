6478d26cdf00e35197e9ad297e7412cff6a68575
Nick Fitzgerald <fitzgen@gmail.com>
2009-08-18 01:33:50 -0700

    adding a list filter for admin on the right

--- admin.py
+++ admin.py
@@ -5,6 +5,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'pub_date'
     list_display = ('title', 'is_published',
                     'is_deleted', 'pub_date')
+    list_filter = ('is_published', 'is_deleted', 'pub_date')
     save_on_top = True
     search_fields = ('title','content')
 admin.site.register(Post, PostAdmin)
