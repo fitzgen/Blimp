a56364973ffe233115fa98747251f94596913fa0
Nick Fitzgerald <fitzgen@gmail.com>
2009-09-28 20:21:09 -0700

    Adding pupdate to feeds.

--- feeds.py
+++ feeds.py
@@ -2,6 +2,8 @@ from django.contrib.syndication.feeds import Feed
 from django.utils.feedgenerator import Atom1Feed
 from models import Post
 
+from datetime import datetime
+
 class RssLatestPosts(Feed):
     author_name = "Nick Fitzgerald"
     author_email = "fitzgen@gmail.com"
@@ -12,6 +14,10 @@ class RssLatestPosts(Feed):
     def items(self):
         return Post.objects.filter(is_published=True,
                                    is_deleted=False).order_by('-pub_date')[:5]
+
+    def item_pubdate(self, item):
+        date = item.pub_date
+        return datetime(date.year, date.month, date.day)
 
 
 class AtomLatestPosts(RssLatestPosts):
