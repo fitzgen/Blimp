08e346bfb568aa711cc7db497336c0ebfe183bde
Nick Fitzgerald <fitzgen@gmail.com>
2010-05-22 13:48:41 -0700

    Removing unnecessary code, now that everything is in Disqus

--- models.py
+++ models.py
@@ -18,65 +18,12 @@ class Post(models.Model):
                                          null=True)
     pub_date = models.DateTimeField()
 
+    def __unicode__(self):
+        return unicode(self.title)
+
     def get_absolute_url(self):
-        return "".join(["http://fitzgeraldnick.com", reverse("blimp_detail", args=[self.id,])])
+        return reverse("blimp_detail", args=[self.id,])
 
     def comments_open(self):
         delta = datetime.datetime.now() - self.pub_date
         return delta.days < 28
-
-#class Link(models.Model):
-#    """A link from Google Reader"""
-#    author = models.CharField(max_length=255)
-#    content = models.TextField()
-#    link = models.URLField(verify_exists=False)
-#    pub_date = models.DateField()
-#    title = models.CharField(max_length=255)
-#
-#    def get_absolute_url(self):
-#        return self.link
-
-
-# Signals
-
-def on_comment(sender, comment, request, *args, **kwargs):
-    akismet = Akismet(agent="http://fitzgeraldnick.com")
-    akismet.setAPIKey(key=settings.AKISMET_API_KEY,
-                      blog_url="http://fitzgeraldnick.com/")
-
-    if akismet.verify_key():
-        data = {
-            'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
-            'user_agent': request.META.get('HTTP_USER_AGENT', ''),
-            'referrer': request.META.get('HTTP_REFERER', ''),
-            'comment_type': 'comment',
-            'comment_author': comment.user_name.encode('utf-8'),
-        }
-
-        try:
-            if akismet.comment_check(comment.comment.encode('utf-8'), data=data, build_data=True):
-                comment.is_public = False
-                comment.save()
-
-                subject = "New comment marked as spam"
-                message = comment.get_as_text()
-
-            else:
-                subject = "New comment"
-                message = comment.get_as_text()
-
-        except AkismetError, e:
-            comment.is_public = False
-            comment.save()
-            mail_admins("AkismetError, Possibly a spam message, comment not public",
-                        comment.get_as_text())
-            raise e
-
-    else:
-        subject = "Akismet api key verification failed"
-        message = subject
-
-    message += "\n\n===============================\n\n%s" % comment.get_as_text()
-    mail_admins(subject, message)
-
-comment_was_posted.connect(on_comment)
